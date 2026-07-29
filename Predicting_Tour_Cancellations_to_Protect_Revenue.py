8001a363c094011f576f551c979c5c955a6de9fe
tanishksharma2510 <296786505+tanishksharma2510@users.noreply.github.com>
2026-07-29 23:43:31 +0530

    Updated file

--- Predicting_Tour_Cancellations_to_Protect_Revenue.py
+++ Predicting_Tour_Cancellations_to_Protect_Revenue.py
@@ -612,5 +612,4 @@ labels=dict(x='Predicted Cancellations',y='Actual Cancellations',color='Count'),
 
 
 if __name__=='__main__':
-    port=int(os.environ.get('PORT',8050))
-    app.run(debug=True,use_reloader=False,host="0.0.0.0",port=port)
+    app.run(debug=False,use_reloader=False,host="0.0.0.0")
